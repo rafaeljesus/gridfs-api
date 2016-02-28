25293117c4db3c05d42ca1b08727c7d4f3560b95
rafaeljesus <rafaelljesus86@gmail.com>
2016-02-28 10:27:37 -0300

    Set NODE_ENV=test on package.json

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import koa from 'koa'
 import cors from 'kcors'
 import helmet from 'koa-helmet'
 import logger from 'koa-logger'
-import serve from 'koa-static'
 
 import homeAPI from './api/home/routes'
 import fileAPI from './api/files/routes'
@@ -16,7 +15,6 @@ app.use(cors({
 app.use(helmet())
 app.use(homeAPI.routes())
 app.use(fileAPI.routes())
-app.use(serve('public'))
 
 app.experimental = true
 
